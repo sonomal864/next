45dc7c5cb6841c52b59f0a656dd3eb93775bdd73
sonomal864 <liveinvain09@gmail.com>
2023-01-03 20:12:57 +0900

    modified:   libs/api.js 	deleted:    next.config.js 	modified:   package.json

--- libs/api.js
+++ libs/api.js
@@ -1,5 +1,5 @@
 import { createClient } from 'microcms-js-sdk';
-
+import fetch from 'isomorphic-unfetch';
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN,
   apiKey: process.env.API_KEY,
